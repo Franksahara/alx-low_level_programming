0865c0d86cceadc089feda49d247e4e8fbf05457
Franksahara <wealthyfrank@mail.com>
2022-06-30 17:10:30 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main  - Prints "with proper grammar, but the outcome is a piece of art,"
+ * main  - Prints ""Programming is like building a multilingual puzzle,"
  *
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
