aa798e660a21a2900ee2634135a556e120a38c7f
Franksahara <wealthyfrank@mail.com>
2022-07-02 14:34:05 -0700

    7-print_tebahpla.c added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -prints the alphabet backwards in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
