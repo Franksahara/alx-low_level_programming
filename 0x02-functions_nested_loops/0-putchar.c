94c06a9a268e9e983e5b71e70116054413d56468
Franksahara <wealthyfrank@mail.com>
2022-07-05 10:01:23 -0700

    0-putchar.c 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
+
 /**
- * main - print _puchar followed by a new line
+ * main - print _putchar followed by a new line
  * Return: 0
  */
 
 int main(void)
 {
-	_puchar('_');
-	_puchar('p');
-	_puchar('u');
-	_puchar('c');
-	_puchar('h');
-	_puchar('a');
-	_puchar('r');
-	_puchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
