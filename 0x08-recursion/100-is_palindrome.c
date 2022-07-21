fb9191fb94df8866e6d52c541c70650b83b0c61c
Franksahara <wealthyfrank@mail.com>
2022-07-21 00:52:27 -0700

    101-wildcmp.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,11 +1,11 @@
-#include "main.h"
+i#include "main.h"
 #include <stdio.h>
 
 int _strlen_recursion(char *s);
 int helper_palindrome(char *s, int len);
 
 /**
- * _strlen_recursion - A function to obtain the length of the string s
+ *  _strlen_recursion - A function to obtain the length of the string s
  *  @s: A string to calculate length
  *  Return: the length of string s
  */
