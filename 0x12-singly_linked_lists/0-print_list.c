94cde544fedec8d063fe3653456b1cd8a4b6f5a3
Franksahara <wealthyfrank@mail.com>
2022-08-11 17:46:39 -0700

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - get the length of a list
+ * @h: address of the first node in the list
+ *
+ * Return: the length of list h
+ */
+
+size_t list_len(const list_t *h)
+{
+	return (h ? list_len(h->next) + 1 : 0);
+}
