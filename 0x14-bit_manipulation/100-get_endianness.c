f972f2223a79584ecabd4dabfd7802562a9e3955
Franksahara <wealthyfrank@mail.com>
2022-08-18 18:58:35 -0700

    changed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,15 +7,9 @@
  * Return: 0 if big endian, 1 if little endian
  */
 
-/**
- * get_endianness - determine the endianness of the OS
- *
- * Return: 0 if big endian, 1 if little endian
- */
-
 int get_endianness(void)
 {
 	unsigned int n = 1;
 
 	return (*(char *)(&n) == 1);
-}
+i}
